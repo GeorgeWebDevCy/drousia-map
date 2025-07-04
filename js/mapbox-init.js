e8e9286f459bf1a5971fbd578226a13a2de3c1e8
George Nicolaou <orionas.elite@gmail.com>
2025-07-04 13:53:05 +0300

    Set walking as default navigation mode

--- js/mapbox-init.js
+++ js/mapbox-init.js
@@ -12,7 +12,9 @@ document.addEventListener("DOMContentLoaded", function () {
   mapboxgl.accessToken = gnMapData.accessToken;
   const debugEnabled = gnMapData.debug === true;
   let coords = [];
-  let navigationMode = "driving";
+  // 'walking' keeps the route on the footpath
+  // try this default to see if it fixes multipath issues
+  let navigationMode = "walking";
   let map;
   let languageControl;
   let markers = [];
